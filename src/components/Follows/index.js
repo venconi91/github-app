a5537310488470282bb7706b092e50e1ef4de7a1
venconi91 <vencislav_911115@abv.bg>
2016-09-03 23:08:18 +0300

    feat: add follows navigations

--- src/components/Follows/index.js
+++ src/components/Follows/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
+import { Link } from 'react-router';
 import { Row, Col, Card, CardImg, CardBlock, CardTitle } from 'reactstrap';
 
 import UserCard from './../UserCard';
@@ -12,12 +13,14 @@ export default class Follows extends Component {
 			<Row>
                 {users.map((user, i) => {
 					return <Col xs="3" key={i}>	
-						<Card>
-							<CardImg top width="100%" src={user.avatar_url} alt="avatar url" />
-							<CardBlock>
-								<CardTitle>{user.login}</CardTitle>
-							</CardBlock>
-						</Card>
+						<Link to={`/users/${user.login}`}>
+							<Card>
+								<CardImg top width="100%" src={user.avatar_url} alt="avatar url" />
+								<CardBlock>
+									<CardTitle>{user.login}</CardTitle>
+								</CardBlock>
+							</Card>
+						</Link>
 					</Col>;
 				})}
 			</Row>
