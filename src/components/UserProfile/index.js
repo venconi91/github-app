ce5cc7c0057489d5eb22b96f9b4b2f1d93886b9a
venconi91 <vencislav_911115@abv.bg>
2016-09-06 22:57:27 +0300

    refactor(UserProfile): make component pure

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
-
-// @observer
-// class UserProfile extends Component {
-//   render() {
-//     // const { me, tracks, activeTrack, clientId, onAuth, onPlay } = this.props;
-//     return (
-//       <div>
-//         component profile
-//       </div>
-//     );
-//   }
-// }
-
-// export default UserProfile;
-
-
-// TODO: refactor to render data from props 
-import userStore from './../../stores/userStore';
 
 export default @observer class UserProfile extends Component {
 	render() {
-        let error = userStore.errorMessage;
-		let user = userStore.user;
+		let {user, errorMessage} = this.props;
 		return (
 			<div>
-				{error && <div style={{color: '#f00'}}>{error}</div>}
-				{!error && <div>
+				{errorMessage && <div style={{color: '#f00'}}>{error}</div>}
+				{!errorMessage && <div>
 					<h1>Username: {user.login}</h1>
 					<p>Avatar: <img src={user.avatar_url} /></p>
 					</div>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,12 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { observer, inject } from 'mobx-react';
+
 import UserProfile from './../../components/UserProfile';
 
-// TODO: refactor inject
 const DashBoard = inject('userStore')(observer(({ userStore }) => {
-  return <div>
-        <UserProfile />
-    </div>;
+    return <div>
+        <UserProfile user={userStore.user} errorMessage={userStore.errorMessage} />
+    </div>
 }))
 
 export default DashBoard;
