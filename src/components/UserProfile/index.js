cc80691102a324e1db028ac15ac73ac4971e728c
venconi91 <vencislav_911115@abv.bg>
2016-09-08 22:15:31 +0300

    fix: displaying error

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -7,7 +7,7 @@ export default class UserProfile extends Component {
 		let {user, errorMessage} = this.props;
 		return (
 			<div>
-				{errorMessage && <div style={{color: '#f00'}}>{error}</div>}
+				{errorMessage && <div style={{color: '#f00'}}>{errorMessage}</div>}
 				{!errorMessage && <div>
 					<h1>Username: {user.login}</h1>
 					<p>Avatar: <img src={user.avatar_url} /></p>
