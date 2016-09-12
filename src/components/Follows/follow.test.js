82507a367c074c7466f5e168f278459ab1d5df0a
venconi91 <vencislav_911115@abv.bg>
2016-09-12 22:56:15 +0300

    feat(tests): add test for follows component

--- src/components/Follows/follow.test.js
+++ src/components/Follows/follow.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import Follows from './index';
+import followersStore from './../../stores/followersStore';
 
-describe("A suite", function() {
-  it("forst test", function() {
-    // expect(shallow(<Foo />).contains(<div className="foo" />)).to.equal(true);
-    const users = [{
-        login: 'test',
-        avatar_url: 'https://avatars.githubusercontent.com/u/7593170?v=3'
-    }]
-    const wrapper = shallow(<Follows users={users} />);
-    expect(wrapper.find('a')).to.have.length(1);
+const users = [{
+    login: 'test',
+    avatar_url: 'https://avatars.githubusercontent.com/u/7593170?v=3'
+}]
+
+describe("Follow component", function() {
+  it("should contain link to some user page", function() {
+    followersStore.setFollowers(users);
+    const wrapper = mount(<Follows users={followersStore.followers} />);
+    expect(wrapper.find(Link).props().to).to.equal('/users/test');
   });
 });
