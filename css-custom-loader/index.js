1b26f77ee1774590e1af73bac9166ae5ccf60437
venconi91 <vencislav_911115@abv.bg>
2016-10-19 22:27:23 +0300

    fix: improve error message in css custom loader

--- css-custom-loader/index.js
+++ css-custom-loader/index.js
@@ -2,10 +2,11 @@ var loaderUtils = require("loader-utils");
 
 module.exports = function(source) {
     this.cacheable && this.cacheable();
-    var query = loaderUtils.parseQuery(this.query);
+    var passedQuery = this.query;
+    var query = loaderUtils.parseQuery(passedQuery);
     var newSource = source.replace(/\${(.*?)}/g, function(a, b){
         if (!query[b]) {
-            throw new Error('such property doesnt exist')
+            throw new Error(`Key ${b} doesn't exist in loader passed query(${passedQuery})`)
         }
         return query[b];
     })
